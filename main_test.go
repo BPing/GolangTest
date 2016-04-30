2f6b49c7647e6a462412a2c86e8d55d21b2dc50e
cbping <452775680@qq.com>
2016-04-30 17:58:51 +0800

    add sign test

--- main_test.go
+++ main_test.go
@@ -3,10 +3,21 @@ package main
 import (
 	"testing"
 )
+
 func TestPush(t *testing.T) {
-	cmd:=newPushCmd("http://","http://","123456")
-	err:=cmd.CmdExec.Run()
-	if nil==err{
+	cmd := newPushCmd("http://", "http://", "123456")
+	err := cmd.CmdExec.Run()
+	if nil == err {
 		t.Fatal("test error")
+	}
+}
+
+func TestSign(t *testing.T) {
+	key = "dw5445gg77gdg4a45"
+	authUser = "4t"
+	signFlag := "4t:20d9bef196a3a65651fec2c3a09128d2"
+	param := map[string]string{"d":"bb"}
+	if (!CheckSign(param, signFlag)) {
+		t.Fatal(Sign(param))
 	}
 }
